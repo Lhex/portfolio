004488f2eff663c2825b66498f82bcbaca8ebb2b
Lhex <xiurenyap@gmail.com>
2015-09-27 00:45:47 +0800

    "Fix overlay width and fb link"

--- Portfolio/js/portfolio.js
+++ Portfolio/js/portfolio.js
@@ -1,5 +1,25 @@
+
+
+//Overlay
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+$overlay.append($image);
+$overlay.append($caption);
+$("body").append($overlay);
+
 $("#gallery li a").click(function(event){
 	event.preventDefault();
-	var href = $(this).attr("href");
-	console.log(href);
+	var imageLocation = $(this).attr("href");
+	$image.attr("src", imageLocation);
+
+	var captionText = $(this).children("img").attr("alt");
+	$caption.text(captionText);
+	
+	$overlay.show();
+});
+
+$overlay.click(function(){
+	$overlay.hide();
 });
